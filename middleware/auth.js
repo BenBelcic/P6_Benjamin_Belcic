799e373bade39f120a5f51c4f31b02eabb2954d2
Benjamin <benjamin.belcic.pro@gmail.com>
2022-11-11 18:07:09 +0100

    add authentification middleware

---
+++ middleware/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split('')[1]; // on récupère le token du header d'authorization avec split (il est en 2eme)
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // on décode le token avec verify, arguments token et la clé secrete
+        const userId = decodedToken.userId;     // on récupère dans le token décodé la propriété userId
+        req.auth = {
+            userId: userId
+        };
+    } catch(error) {
+        res.status(401).json({ error });
+    }
+};
--- routes/sauce.js
+++ routes/sauce.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const auth = require('auth');
 const router = express.Router();    // création de router
+
 const sauceCtrl = require('../controllers/sauce');
 
-router.post('/', sauceCtrl.createSauce);        // création d'une sauce en fonction du schéma
-router.get('/', sauceCtrl.getAllSauces);        // récupération de la liste de toutes les sauces
-router.get('/:id', sauceCtrl.getOneSauce);      // récupération d'une sauce précise en fonction de son id
-router.put('/:id', sauceCtrl.modifySauce);      // mise à jour d'une sauce
-router.delete('/:id', sauceCtrl.deleteSauce);   // suppression d'une sauce
+router.post('/', auth, sauceCtrl.createSauce);        // création d'une sauce en fonction du schéma
+router.get('/', auth, sauceCtrl.getAllSauces);        // récupération de la liste de toutes les sauces
+router.get('/:id', auth, sauceCtrl.getOneSauce);      // récupération d'une sauce précise en fonction de son id
+router.put('/:id', auth, sauceCtrl.modifySauce);      // mise à jour d'une sauce
+router.delete('/:id', auth, sauceCtrl.deleteSauce);   // suppression d'une sauce
 
 module.exports = router;
