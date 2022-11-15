b8295464cfcd0c8be3d0a8582135fb6b0fc6eaf8
Benjamin <benjamin.belcic.pro@gmail.com>
2022-11-15 03:19:02 +0100

    correcting auth routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split('')[1]; // on récupère le token du header d'authorization avec split (il est en 2eme)
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // on décode le token avec verify, arguments token et la clé secrete
         const userId = decodedToken.userId;     // on récupère dans le token décodé la propriété userId
-        req.auth = {
+        req.auth = {                            // on crée
             userId: userId
         };
     } catch(error) {
--- routes/sauce.js
+++ routes/sauce.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const auth = require('auth');
+const auth = require('../middleware/auth');
 const router = express.Router();    // création de router
 
 const sauceCtrl = require('../controllers/sauce');
